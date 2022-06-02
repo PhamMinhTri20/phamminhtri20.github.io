6f5d93503340dfdecbd4feb6dde3e34e3523b44b
Phạm Minh Trí <92964388+PhamMinhTri20@users.noreply.github.com>
2022-06-03 00:58:00 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 50) {
+            $(".navbar").addClass("sticky-nav");
+        } else {
+            $(".navbar").removeClass("sticky-nav");
+        }
+    });
+
+    // BACKSTRETCH SLIDESHOW
+    $('#section_1').backstretch([
+      "images/slide/bien.jpg", 
+      "images/slide/bien1.jpg",
+      "images/slide/bien2.jpg"
+    ],  {duration: 2000, fade: 750});
+    
+  })(window.jQuery);
+
+
